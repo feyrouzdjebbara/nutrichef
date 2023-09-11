f1a78f833d6a0437e97e245461644b045ca0b534
feyrouzdjebbara <feyrouzdjebbara2000@gmail.com>
2023-09-11 08:48:53 -1100

    commit

---
+++ pages/home.js
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import styles from "../styles/home.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+    
+        <div className={styles.count}>
+          <div className={styles.periodcount}></div>
+          <div className={styles.containerexin}>
+              <div className={styles.expencescount}></div>
+              <div className={styles.incomecount}></div>
+          </div>
+        </div>
+        
+      </header>
+
+      <main className={styles.main}>
+      
+
+
+
+      </main>
+
+      <footer className={styles.footer}>
+          <Link href="/">
+          <button>Home</button>
+        </Link>
+        <Link href="/about">
+          <button>Chart</button>
+        </Link> 
+     
+      </footer>
+    </div>
+  );
+}
