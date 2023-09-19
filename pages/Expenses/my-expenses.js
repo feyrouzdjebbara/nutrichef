dc66093d8cb52e708b74a142320784ab6dae6bbf
feyrouzdjebbara <feyrouzdjebbara2000@gmail.com>
2023-09-19 09:32:27 -1100

    commit f

--- pages/Expenses/my-expenses.js
+++ pages/Expenses/my-expenses.js
@@ -1,26 +1,126 @@
 import Link from "next/link";
 import styles from "../../styles/expenses.module.css";
 import { useRouter } from 'next/router';
-import { useEffect } from "react"; 
+import { useEffect ,useState} from "react"; 
 import jwt from 'jsonwebtoken'; 
+import axios from 'axios';
+
 
 export default function Home() {
   const router = useRouter();
-  useEffect(() => {
-    const jwtToken = localStorage.getItem('OursiteJWT'); 
+   const [expenses, setExpenses] = useState([]);
+   const [expensesSum, setExpensesSum] = useState(0); 
+   const [expensesMonth, setExpensesMonth] = useState(0); 
+   const [expensesYear, setExpensesYear] = useState(0); 
+   const [TotalYearExpenses, setTotalYearExpenses] = useState(0);
+   const [selectedExpense, setSelectedExpense] = useState(null);
+   const [selectedOption, setSelectedOption] = useState('totalExpenses');
+
+   const handleExpenseClick = (expenseId) => {
+    setSelectedExpense(expenseId);
+  };
+  const handleSelectChange = (e) => {
+    setSelectedOption(e.target.value);
+  };
+
+  const handleDeleteClick = (expenseId) => {
+    const deleteEndpoint = `http://localhost:3333/expenses/${expenseId}`;
+    const jwtToken = localStorage.getItem('OursiteJWT');
     const decodedToken = jwt.decode(jwtToken);
-   console.log(decodedToken)
+    const headers = {
+      Authorization: `Bearer ${jwtToken}`,
+    };
+    
+    axios
+    .delete(deleteEndpoint, { headers })
+    .then((response) => {
+      console.log('Expense deleted successfully:', response.data);
+
+      axios
+      .get(`http://localhost:3333/expenses/sum/${decodedToken.id}`, {
+        headers: headers,
+      })
+      .then((response) => {
+        setExpensesSum(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching expenses sum:', error);
+      });
+      setExpenses((prevExpenses) =>
+        prevExpenses.filter((expense) => expense._id !== expenseId)
+      );
+    })
+    .catch((error) => {
+     
+      console.error('Error deleting expense:', error);
+    });
+  };
+  
+  const handleUpdateClick = (expenseId) => {
+    
+    router.push("/update-expense");
+  };
+  
+
+
+   useEffect(() => {
+    const jwtToken = localStorage.getItem('OursiteJWT');
+    const decodedToken = jwt.decode(jwtToken);
+
     if (!jwtToken) {
       router.push('/login');
     } else {
-
-
-     
+      console.log(decodedToken)
+      const headers = {
+        Authorization: `Bearer ${jwtToken}`,
+      };
       
+      axios.get(`http://localhost:3333/expenses/my-expenses/${decodedToken.id}`, {
+          headers: headers,
+        })
+        .then((response) => {
+          setExpenses(response.data);
+        })
+        .catch((error) => {
+          console.error('Error fetching expenses:', error);
+        });
 
+        axios
+        .get(`http://localhost:3333/expenses/sum/${decodedToken.id}`, {
+          headers: headers,})
+          .then((response) => {
+            setExpensesSum(response.data);
+           
+          })
+          .catch((error) => {
+            console.error('Error fetching expenses sum:', error);
+          });
+
+          axios
+        .get(`http://localhost:3333/expenses/current-month/${decodedToken.id}`, {
+          headers: headers,})
+          .then((response) => {
+            setExpensesMonth(response.data);
+           
+          })
+          axios
+          .get(`http://localhost:3333/expenses/year/${decodedToken.id}`, {
+            headers: headers,
+          })
+          .then((response) => {
+            const expensesYearData = response.data;
+            const totalYearExpenses = Object.values(expensesYearData).reduce(
+              (total, monthExpense) => total + monthExpense,
+              0
+            );
+            setExpensesYear(expensesYearData);
+            setTotalYearExpenses(totalYearExpenses); 
+          })
+          .catch((error) => {
+            console.error('Error fetching yearly expenses:', error);
+          });
     }
   }, [router]);
-
 
 
 
@@ -32,26 +132,72 @@ export default function Home() {
     
         <div className={styles.count}>
           <div className={styles.periodcount}> 
-          <select className={styles.periodselect} value="thisMonth">
-            <option value="thisMonth">This Month</option>
-            <option value="thisYear">This Year</option> 
+          <select
+          className={styles.periodselect}
+          value={selectedOption}
+          onChange={handleSelectChange}
+        >
+            <option value="totalExpenses">Total Expenses</option>
+              <option value="thisMonth">This Month</option>
+              <option value="thisYear">This Year</option>
           </select>
           </div>
           <div className={styles.containerexin}>
-              <div className={styles.expencescount}></div>
-              <div className={styles.incomecount}></div>
-          </div>
+        {selectedOption === 'totalExpenses' ? (
+          <p>Total Expenses: {expensesSum} DZD</p>
+        ) : selectedOption === 'thisMonth' ? (
+          <p>This Month Expenses: {expensesMonth} DZD</p>
+        ) : selectedOption === 'thisYear' ? (
+          <p>This Year Expenses: {TotalYearExpenses} DZD</p>
+        ) : null}
+      </div>
         </div>
         
       </header>
 
       <main className={styles.main}>
       
-      <h1>Welcome,!</h1>
+    
       <Link href="/Expenses/add-expense">
         <button className={styles.addExpensesButton}>+</button>
       </Link>
+<div className={styles.expenses} >
+      <ul className={styles.expenselist}>
+          {expenses.map((expense) => (
+          
+            <li key={expense._id} className={styles.expenseItem} onClick={() => handleExpenseClick(expense._id)}>
+              <div className={styles.expenseTop}>
+                <p className={styles.expenseTitle}>Title: {expense.title}</p>
+                <p className={styles.expenseDate}>
+               {new Date(expense.date).toLocaleDateString('en-US', {
+                    day: 'numeric',
+                    month: 'numeric',
+                    year: 'numeric',
+                  })}
+                </p>
+                
+              </div>
+              <p>{expense.description}</p>
+              <div className={styles.expenseBottom}>
+                <div className={styles.expenseLeft}>
+                <p className={styles.expenseAmount}>Amount: {expense.amount}</p>
+                </div>
+                <div className={styles.expenseRight}>
+                <p className={styles.expenseCategory}>Category: {expense.category}</p>
+                
+                </div>
+              </div>
+              {selectedExpense === expense._id && (
+                <div>
+                  <button  className={styles.update} onClick={() => handleUpdateClick(expense)}>Update</button>
+                  <button className={styles.delete} onClick={() => handleDeleteClick(expense._id)}>Delete</button>
+                </div>
+              )}
+            </li>
+          ))}
+        </ul>
 
+</div>
       </main>
 
       <footer className={styles.footer}>
---
+++ pages/Expenses/update-expense.js
@@ -0,0 +1,173 @@
+import styles from "../../styles/addExpense.module.css";
+import { useState  } from 'react';
+import Link from 'next/link';
+import axios from 'axios';
+import { useRouter } from "next/router";
+
+export default function AddExpense() {
+
+    const router = useRouter();
+
+    const [expenseData, setExpenseData] = useState({
+        title: '',
+        category: '',
+        amount: '',
+        date: new Date().toISOString().split('.')[0],
+        description: '',
+      });
+     
+      const handleChange = (e) => {
+        const { name, value } = e.target;
+        
+         
+            setExpenseData({
+              ...expenseData,
+              [name]: name === 'amount' ? parseFloat(value) : value,
+            });
+          
+        };
+      
+
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+      
+        try {
+          const Expense= {
+            title: expenseData.title,
+            amount: expenseData.amount,
+            date: expenseData.date,
+            category: expenseData.category,
+            description: expenseData.description,
+          };
+
+          if (typeof window !== 'undefined') {
+             console.log(expenseData.date)
+            const authToken = localStorage.getItem('OursiteJWT');
+            const headers = {
+              Authorization: `Bearer ${authToken}`,
+            };
+
+           
+              
+          const response = await axios.post('http://localhost:3333/expenses/add', 
+          Expense ,{ headers });
+    
+          if (response.status === 201) {
+            console.log('Expense added successfully:', response.data);
+            router.push('/Expenses/my-expenses');
+          } else {
+            console.error('Failed to add expense:', response.status, response.data);
+          }
+        }
+    } catch (error) {
+        if (error.response) {
+          console.error('Error adding expense - Response Data:', error.response.data);
+          console.error('Error adding expense - Status:', error.response.status);
+        } else if (error.request) {
+          console.error('Error adding expense - Request:', error.request);
+        } else {
+          console.error('Error adding expense - Message:', error.message);
+        }
+      }
+    };
+      
+     
+      
+      
+      
+      
+
+  return (
+    <>
+    
+    <div className={styles.home}>
+    <Link  href="/Expenses/my-expenses">
+    <img src="/img/back.png" alt="Home" />
+    </Link>
+    </div>
+    
+     <div className={styles.container}>
+    
+      <h1 className={styles.title}>Update Expense.</h1>
+    
+    <div> 
+      <form className={styles.formstyle} onSubmit={handleSubmit}>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={expenseData.name}
+            className={styles.inputstyle}
+            onChange={handleChange}
+            placeholder="Title"
+            required
+          />
+        <br/>
+          <select 
+            id="category"
+            name="category"
+            value={expenseData.category}
+            onChange={handleChange}
+            className={styles.inputstyle}
+            placeholder="Select a category"
+            required
+          >
+            <option  value="" disabled >
+              Select a category
+            </option>
+            <option value="Food">Food</option>
+            <option value="Bills">Bills</option>
+            <option value="Travel">Travel</option>
+            <option value="Entertainment">Entertainment</option>
+            <option value="Transportation">Transportation</option>
+            <option value="Other">Other</option>
+            
+          </select>
+          <br/>
+          
+          <input
+            type="text"
+            id="amount"
+            name="amount"
+            value={expenseData.amount}
+            className={styles.inputstyle}
+            onChange={handleChange}
+            placeholder="Amount"
+            required
+          />
+       <br/>
+       <input
+            type="date"
+            id="date"
+            name="date"
+            value={expenseData.date} 
+            onChange={handleChange}
+            className={styles.inputstyle}
+            placeholder="Date"
+            />
+           <br/>
+          <textarea
+            id="description"
+            name="description"
+            value={expenseData.description}
+            onChange={handleChange}
+            className={styles.inputstyle}
+            placeholder="Description"
+          />
+          <br/>
+          <input
+          type="submit"
+          value="Update Expense"
+          className={styles.subbutton}
+        />
+        
+       
+      </form>
+      
+    </div>
+   
+       
+    </div>
+      </>
+  );
+}
