60fece1362402e7514a526f74be106ef2fab0282
feyrouzdjebbara <feyrouzdjebbara2000@gmail.com>
2024-01-18 15:24:25 -1100

    Your commit message

--- pages/Home.js
+++ pages/Home.js
@@ -2,16 +2,20 @@
 import React from 'react'
 import Food from './Food'
 import { useRouter } from 'next/router';
+const router = useRouter();
 
 function home() {
-    const router = useRouter();
-    const user = localStorage.getItem('userId');
-  
+
+    useEffect(() => {
+       
+        if (typeof window !== 'undefined') {
+          const user = localStorage.getItem('userId');
     
-    if (!user) {
-     
-     router.push('/login');
-     }
+          if (!user) {
+            router.push('/login');
+          }
+        }
+      }, [router]);
         
   
 
